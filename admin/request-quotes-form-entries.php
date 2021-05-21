6f8fbb0e5a66498cbba75bece0a65ab3484f12d1
jhomel <jhomignacio08@gmail.com>
2021-05-21 13:24:54 +0800

    fix the table for request quotes entries

--- admin/request-quotes-form-entries.php
+++ admin/request-quotes-form-entries.php
@@ -18,9 +18,32 @@
 		global $wpdb;
 		$request_form_quote_entries = $wpdb->prefix.'request_quotes_entries';
 		$result = $wpdb->get_results("SELECT * FROM $request_form_quote_entries");
-		echo '<pre>';
-		print_r($result);
-		echo '</pre>';
-
+		?>
+		<div class="form-entries-container">
+			<h1 class="form-entries request">Quote Request Entries</h1>
+			<table class="form-entries-table" id="form-entries">
+				<tr  class="form-entries-tr">
+					<!-- <th class="form-entries-th-col-1">Actions</th> -->
+					<th class="form-entries-th-col-1">Subject</th>
+					<th class="form-entries-th-col-2">Sender</th>
+					<th class="form-entries-th-col-3">Sent to</th>
+					<th class="form-entries-th-col-3">Delivery Time</th>
+				</tr>
+				<?php
+				foreach($result as $email_logs):
+				?>
+				<tr>
+					<!-- <td style="width:10%;"></td> -->
+					<td><?=$email_logs->subject_title;?></td>
+					<td><?=$email_logs->sender;?></td>
+					<td><?=$email_logs->sent_to;?></td>
+					<td><?=$email_logs->created_at;?></td>
+				</tr>
+				<?php
+				endforeach;
+				?>
+			</table>
+		</div>
+		<?php
 	?>
 </div>
